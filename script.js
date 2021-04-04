82d7065195dd28c1bebe94318e9c12ef5bc44abc
Arjun Krishna <arjun12345krishna@gmail.com>
2021-04-05 01:32:49 +0530

    hexInput and inputColor

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var hexInput = document.getElementById('hexInput');
+var inputColor = document.getElementById('inputColor');
+
+hexInput.addEventListener('keyup', () => {
+    var hex = hexInput.value;
+    if (!isValidHex(hex))
+        return;
+
+    var strippedHex = hex.replace('#', '');
+    inputColor.style.backgroundColor = "#" + strippedHex;
+})
+
 var isValidHex = (hex) => {
     if (!hex) {
         return false;
@@ -6,4 +18,3 @@ var isValidHex = (hex) => {
     // console.log(strippedHex);
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
-
