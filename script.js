1096778444435ab9aac332d8b4af4f3df87a44fc
Arjun Krishna <arjun12345krishna@gmail.com>
2021-04-05 02:29:16 +0530

    var convertHexToRGB = (hex) => {

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ hexInput.addEventListener('keyup', () => {
     inputColor.style.backgroundColor = "#" + strippedHex;
 })
 
+
 var isValidHex = (hex) => {
     if (!hex) {
         return false;
@@ -18,3 +19,24 @@ var isValidHex = (hex) => {
     // console.log(strippedHex);
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
+
+
+var convertHexToRGB = (hex) => {
+    if (!hex) {
+        return;
+    }
+    var strippedHex = hex.replace('#', '');
+
+    if (strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0] +
+            strippedHex[1] + strippedHex[1] +
+            strippedHex[2] + strippedHex[2];
+    }
+    // console.log(strippedHex);
+    var r = parseInt(strippedHex.substring(0,2), 16);
+    var g = parseInt(strippedHex.substring(2,4), 16);
+    var b = parseInt(strippedHex.substring(4,6), 16);
+    return {r, g, b};
+}
+
+// console.log(convertHexToRGB("ffe"));
